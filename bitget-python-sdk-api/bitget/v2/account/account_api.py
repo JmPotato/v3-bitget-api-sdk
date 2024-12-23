999c816ae73e49dbaaf2ba59e57492d166b6bf47
JmPotato <ghzpotato@gmail.com>
2024-12-23 10:52:40 +0800

    Add the account v2 API
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

---
+++ bitget-python-sdk-api/bitget/v2/account/account_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from bitget.client import Client
+from bitget.consts import GET
+
+
+class AccountApi(Client):
+    def __init__(
+        self, api_key, api_secret_key, passphrase, use_server_time=False, first=False
+    ):
+        Client.__init__(
+            self, api_key, api_secret_key, passphrase, use_server_time, first
+        )
+
+    def all_account_balance(self):
+        return self._request_with_params(GET, "/api/v2/account/all-account-balance", {})
